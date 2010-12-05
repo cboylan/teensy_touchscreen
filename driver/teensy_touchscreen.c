e55dce7a4c41bae93787507ba1bc14eaea175d88
Clark Boylan <clark.boylan@gmail.com>
2010-12-05 15:18:05 -0800

    Add GPL copyright file and source code headers.

--- driver/teensy_touchscreen.c
+++ driver/teensy_touchscreen.c
@@ -1,7 +1,24 @@
-/* Code adapted from Essential Linux Device Drivers and
-   linux/drivers/hid/usbhid/usbmouse.c (The probe, disconnect, open, and close
-   calls have been copied and adapted from the usbhid mouse driver).
-*/
+/*  Driver for the teensy touchscreen firmware.
+    Copyright (C) 2010 Clark Boylan, Nate Georgeson, Chris Noonan, Abdelhalim
+    Ragab.
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+    Code adapted from linux/drivers/hid/usbhid/usbmouse.c (the probe,
+    disconnect, open, and close calls have been copied and adapted from the
+    usbhid mouse driver). */
 
 #include <linux/module.h>
 #include <linux/usb.h>
--- driver/teensy_touchscreen.h
+++ driver/teensy_touchscreen.h
@@ -1,3 +1,25 @@
+/*  Driver for the teensy touchscreen firmware.
+    Copyright (C) 2010 Clark Boylan, Nate Georgeson, Chris Noonan, Abdelhalim
+    Ragab.
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+    Code adapted from linux/drivers/hid/usbhid/usbmouse.c (the probe,
+    disconnect, open, and close calls have been copied and adapted from the
+    usbhid mouse driver). */
+
 #define TEENSY_TOUCHSCREEN_VENDOR_ID  0x16C0	/* Manufacturer’s Vendor ID */
 #define TEENSY_TOUCHSCREEN_PRODUCT_ID 0x0FFF	/* Device’s Product ID */
 
